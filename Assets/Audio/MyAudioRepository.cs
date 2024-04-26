e26bcf9dfe7feed4a385823fc613888d91e0103f
lemon-1000 <lemon-1000@users.noreply.github.com>
2024-04-27 00:32:36 +0530

    Added audio files, Implemented - jump, ladder, attacks, footsteps and music

--- Assets/Audio/MyAudioRepository.cs
+++ Assets/Audio/MyAudioRepository.cs
@@ -7,7 +7,17 @@ namespace Codebase.Audio
 {
     public partial class AudioRepository : ScriptableObject
     {
-        public AudioClipReference playerFootSteps;
+        public AudioClipReference[] playerFootSteps;
+        public AudioClipReference playerShortAttack;
+        public AudioClipReference playerLongAttack;
+        public AudioClipReference playerJump;
+        public AudioClipReference playerDead;
+        public AudioClipReference playerDamageTaken;
+        public AudioClipReference music;
+        public AudioClipReference playerFallHit;
+        public AudioClipReference healthReduce;
+        public AudioClipReference[] ladderClimb;
+        public AudioClipReference dash;
     }
 }
 
---
+++ Assets/Scripts/AudioScripts.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Codebase.Core;
+using Codebase.Audio;
+
+public class AudioScripts : MonoBehaviour
+{
+    public void AnimEvent_PlayFootsteps()
+   {
+      int Index = UnityEngine.Random.Range(0, AudioManager.Instance.Audios.playerFootSteps.Length);
+      AudioManager.Instance.PlayOneShotSFX(AudioManager.Instance.Audios.playerFootSteps[Index],AudioChannelData.CHANNEL_2);      
+   }
+
+   private void AnimEvent_PlayClimbSFX()
+   {
+      int Index = UnityEngine.Random.Range(0, AudioManager.Instance.Audios.ladderClimb.Length);
+      AudioManager.Instance.PlayOneShotSFX(AudioManager.Instance.Audios.ladderClimb[Index],AudioChannelData.CHANNEL_2);      
+   }
+
+}
